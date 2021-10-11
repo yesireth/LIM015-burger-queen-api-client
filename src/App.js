86c3d02855809440b8b73239c619e6a1466b0278
yesireth <yesireth88@gmail.com>
2021-10-11 18:53:39 -0500

    Se le agrego carpeta para trabajar la primera vista del login

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
+import logo from './assets/logo-bq.png';
 import './App.css';
+import Loging from './componets/LogIn'
 
 function App() {
   return (
@@ -7,16 +8,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <Loging/>
+
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
       </header>
     </div>
   );
---
+++ src/componets/LogIn.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function LogIn() {
+    return (
+        <form>
+  <label>
+    Email:
+    <input type="text" name="name" />
+  </label>
+  <label>
+    Contraseña:
+    <input type="text" name="name" />
+  </label>
+  <input type="submit" value="Submit" />
+  </form>
+    )
+}
+
+ // rfc
