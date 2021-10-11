efe33dbad4ab97e86dbd24bd924edb51d8c86135
yesireth <yesireth88@gmail.com>
2021-10-11 11:25:18 -0500

    Se agrego el api con el fetch

---
+++ burger-queen/src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+const prueba = ()=> {
+fetch('https://chamaburger.herokuapp.com/').then(result => console.log(result))
+// .then(resol => resol.json()).then(resul => console.log(resul))
+}
+
+
+prueba()
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
