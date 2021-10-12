7e7e9ad3c6a642fe5d89bea1eb711bf1b6116386
gisselP <https://github.com/gisselP>
2021-10-12 18:33:27 -0500

    arreglo de conflictos

--- burger-queen/src/app/app.module.ts
+++ burger-queen/src/app/app.module.ts
@@ -3,21 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { HoliComponent } from './components/holi/holi.component';
-
-@NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    HoliComponent
-=======
 
 @NgModule({
   declarations: [
     AppComponent
->>>>>>> upstream/loginAngular
   ],
   imports: [
     BrowserModule,
