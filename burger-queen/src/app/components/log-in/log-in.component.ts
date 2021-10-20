22fcb031a636f8b2b49d3f7c9f4cb39815fffd74
yesireth <yesireth88@gmail.com>
2021-10-20 17:54:26 -0500

    Se resolvio conflictos
    los conflictos eran solo espacios

--- burger-queen/src/app/components/log-in/log-in.component.ts
+++ burger-queen/src/app/components/log-in/log-in.component.ts
@@ -9,10 +9,6 @@ import { AuthService } from '../../services/auth.service';
   styleUrls: ['./log-in.component.css']
 })
 export class LogInComponent implements OnInit {
-<<<<<<< HEAD
-=======
-
->>>>>>> c0314b27ff4034a51279117c949542fdd44157cf
   response: any;
   logInForm = new FormGroup({
     email: new FormControl('',Validators.required),
@@ -29,25 +25,6 @@ export class LogInComponent implements OnInit {
       this.router.navigate(['/menu'])
     }
   }
-<<<<<<< HEAD
-   login() {
-
-    this.AuthService.Auth(this.logInForm.value).subscribe(
-      data => {
-        localStorage.setItem('token',data.token)
-        console.log('success', data)
-        this.router.navigate(['/menu'])
-      },
-      error => {
-        if(error.status > 400){
-        alert('Por favor verifIque los campos')
-        }
-        else if (error.status == 400){
-          alert('Por favor complete los campos')
-        }
-      })
-   } 
-=======
   login() {
   this.AuthService.Auth(this.logInForm.value).subscribe(
     data => {
@@ -64,5 +41,4 @@ export class LogInComponent implements OnInit {
       }
     })
   } 
->>>>>>> c0314b27ff4034a51279117c949542fdd44157cf
 }
