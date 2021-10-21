678e3d845f250848f61425df2428dd8cfb2339dd
yesireth <yesireth88@gmail.com>
2021-10-21 11:04:41 -0500

    Se agrego la funcionalidad de bajar productos

--- burger-queen/src/app/services/products.service.ts
+++ burger-queen/src/app/services/products.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams, HttpHeaders} from '@angular/common/http';
+import { HttpClient, HttpParams} from '@angular/common/http';
 import { Observable} from 'rxjs'
 
 @Injectable({
@@ -8,7 +8,6 @@ import { Observable} from 'rxjs'
 export class ProductService {
   private Url = "https://chamaburger.herokuapp.com/products"; // URL to web api
   private parameters = new  HttpParams();
-  private headers = new HttpHeaders();
   private token: any;
 
   constructor(private http: HttpClient) {  }
@@ -16,13 +15,9 @@ export class ProductService {
     //definen los headers
     this.token = localStorage.getItem('token')
     console.log(this.token)
-    this.headers.set("Content-Type", 'application/json; charset=utf-8');
-    this.headers.set("token", this.token);
-    //definen parametros si hubiera
-    //this.parameters.set('nombreparametro1','valorparametro1')
-    // const headersss= new HttpHeaders({
-    //   'token':'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxNjQ1ZGNiZWZmMGQ3NGFlZTA5MTBkZiIsImlhdCI6MTYzNDc2NjU5MiwiZXhwIjoxNjM0ODUyOTkyfQ.pAsFqL5tizKOnZCgMz54aQxzYwsfIiADi45w0j4_J0M'
-    // })
-    return this.http.get(this.Url,{headers: this.headers})
+    const config = {
+      headers: { token: this.token }
+  };
+    return this.http.get(this.Url, config)
   }
 }
