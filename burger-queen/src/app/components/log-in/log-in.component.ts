23f4cbbceb6feda7100c438895daa00fa7e3e382
gisselP <https://github.com/gisselP>
2021-10-25 16:52:57 -0500

    se agregó la función para agregar el contador

--- burger-queen/src/app/components/log-in/log-in.component.ts
+++ burger-queen/src/app/components/log-in/log-in.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormGroup,FormControl, Validators, FormGroupName, FormControlName} from '@angular/forms'
+import { FormGroup,FormControl, Validators} from '@angular/forms'
 import { AuthService } from '../../services/auth.service';
 
 @Component({
