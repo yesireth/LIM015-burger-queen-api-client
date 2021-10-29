86414709eb14125e3b91b1d38c5a5b8e3fce30ac
yesireth <yesireth88@gmail.com>
2021-10-29 15:19:17 -0500

    Se agrego el envio de las ordenes al mogoDB
    Se envia las ordenes de los clientes para que se prepare la ordenes

--- burger-queen/src/app/models/product-model.ts
+++ burger-queen/src/app/models/product-model.ts
@@ -4,8 +4,18 @@ export interface ProductI {
     price: number;
     type: string;
   }
-export interface ItemI {
-  product: ProductI;
-  amount: number;
-  userName: string;
+
+//NUEVO
+export class Order{
+    userId : string = ""; //usuario logeado
+    client : string= ""; //userName del input
+    status : string= "";
+    products :  OrderDetail[] = [];
+}
+
+export class OrderDetail {
+   qty: number = 0;
+   product: string = ""; //id del producto
+   price :number = 0;
+   productName = ""; 
 }
