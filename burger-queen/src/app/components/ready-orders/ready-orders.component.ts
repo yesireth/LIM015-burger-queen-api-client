1bc2b571c07e937a7ed0194e515166c6da791cd6
yesireth <yesireth88@gmail.com>
2021-11-02 18:55:10 -0500

    Se agrego la funcionalidad de mostrar las ordenes de pendientes a listas

--- burger-queen/src/app/components/ready-orders/ready-orders.component.ts
+++ burger-queen/src/app/components/ready-orders/ready-orders.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { OrderService} from '../../services/orders.service';
 import { Order } from '../../models/product-model';
-
 @Component({
   selector: 'app-ready-orders',
   templateUrl: './ready-orders.component.html',
@@ -9,13 +8,22 @@ import { Order } from '../../models/product-model';
 })
 export class ReadyOrdersComponent implements OnInit {
   orders: Array<any> = [];
-  objOrder = new Order;
+  ordersFilter: Array<any> = [];
+
   constructor(private orderService : OrderService) { }
 
   ngOnInit(): void {
+    this.orderService.getOrders().subscribe((element) => {
+      this.orders = element;
+      this.ordersFilter = this.orders.filter((itens) => itens.status === 'delivering')
+      console.log(this.orders);
+    });
+  }
+
+
+
     /* this.orderService.getOrderReady(this.objOrder).subscribe((element) => {
       this.objOrder = element;
-      console.log(element);
+      console.log(element,8888);
     }); */
   }
-}
